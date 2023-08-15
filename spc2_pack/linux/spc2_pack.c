9e9b4ed1d8fb2a5f11ca065b59857cb972b76924
Terry Janas <tjanas@gmail.com>
2023-08-15 14:11:00 -0500

    spc2_pack: 0.4 -> 0.5 (#6)

--- spc2_pack/linux/spc2_pack.c
+++ spc2_pack/linux/spc2_pack.c
@@ -1,7 +1,7 @@
 //
-// spc2_pack 0.2
-// marshallh
-// 1/18/2012
+// spc2_pack 0.5
+// marshallh, CaitSith2, tjanas
+// 2023-08-15
 //
 
 #include <algorithm>
@@ -69,7 +69,7 @@ std::vector< std::string > glob(const std::string& pattern)
 
 			auto it = filenames_no_case_sort.insert(std::make_pair(key, value));
 			if (!it.second) // insert failed; key already exists
-			{   // Example: warn if both "Filename.spc" and "FileName.spc" exist
+			{	// Example: warn if both "Filename.spc" and "FileName.spc" exist
 				std::cout << "Ignoring file[" << value << "] since it conflicts with [" << it.first->second << "]" << std::endl;
 			}
 		}
@@ -109,12 +109,12 @@ int main(int argc, char* argv[])
 	memset(&filelen, 0, sizeof(filelen));
 	memset(&sp2filename, 0, sizeof(sp2filename));
 
-	printf("\n spc2_pack 0.4\n-------------------------------------------\n");
+	printf("\n spc2_pack 0.5 (2023-08-15)\n-------------------------------------------\n");
 
 	printf(" Packs multiple Super Nintendo SPC sound\n");
 	printf(" files to a single SPC2 \n\n");
 	printf(" SPC2 format by kevtris, packer by marshallh\n");
-	printf(" mods to packer by CaitSith2\n\n");
+	printf(" mods to packer by CaitSith2 and tjanas\n\n");
 	if (argc < 2)
 	{
 		printf(" Error : No directory specified\n");
