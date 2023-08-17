314eb2cf61011e0deccd6c8fcdb148707fcea468
Terry Janas <tjanas@gmail.com>
2023-08-17 14:20:06 -0500

    Delete spc2_write.h

--- spc2_pack/spc2_write.h
+++
@@ -1,7 +0,0 @@
-
-// poopstain
-int spc2_start();
-int spc2_finish(int *final_size, char *filename, u16 num_spc);
-int spc2_write_header(u16 num_spc);
-int spc2_write_metadata(char *filename, spc2_metadata *o, spc_struct *s, spc_idx6_table *t);
-int spc2_write_spc(char *filename, spc_struct *s, spc_idx6_table *t);
